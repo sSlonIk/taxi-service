50fb51a6ad5e8315a462eda0ceaf5952021c8289
sSlonIk <kryachok@gmail.com>
2021-07-29 15:40:58 +0300

    fixed some web pages and @WebServlet

--- src/main/java/cab/controller/IndexController.java
+++ src/main/java/cab/controller/IndexController.java
@@ -1,11 +1,13 @@
 package cab.controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/")
 public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
